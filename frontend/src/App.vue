c451ff6bf70344f0a3a69290462cc20f12e35b18
ARTHUR AZOULA <arthur.azoula@adeo.com>
2024-10-10 11:55:46 +0200

    feat: front clock in clock out

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,3 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { userService } from './service/userService'
-
-const users = ref([])
-
-onMounted(() => {
-  userService.getUsers().then((response) => {
-    users.value = response.data
-  })
-})
-</script>
-
 <template>
-  <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">{{ user.email }}</li>
-    </ul>
-  </div>
+    <router-view></router-view>
 </template>
-
-<style scoped>
-
-</style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                button: {
+                    100: '#544407',
+                    200: '#FA9115',
+                    300: '#FFEAD2',
+                },
+            },
+        },
     },
     plugins: [],
 }

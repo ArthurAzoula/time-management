ed816b8087cc43d06f768b32bbfcf8c3461f16db
Edwin <65991865+xRezio@users.noreply.github.com>
2024-10-14 10:59:16 +0200

    Update vite.config.js
    fix(): Added comment to explain host execution problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Seulement en sudo ( sudo npm run dev )
     port: 80,
     open: true
   }

2e6d0c80677c4e5ef0c0e711443eae2928219462
root <root@devops-phenix.europe-west9-c.c.resolute-arcana-438309-u5.internal>
2024-10-14 08:50:45 +0000

    feat(): Deployed backend/frontend on web - Refer to GCP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    open: true
+  }
 })
